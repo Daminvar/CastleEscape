d3be2244b9627c7ac50ab6b315c7f81d6f61ba3a
Dennis Honeyman <dmh1117@rit.edu>
2010-03-22 19:44:49 -0400

    Removed XBox 360-specific code.

--- CastleEscape/game1.cs
+++ CastleEscape/game1.cs
@@ -69,12 +69,7 @@ namespace CastleEscape
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-                this.Exit();
-
             // TODO: Add your update logic here
-
             base.Update(gameTime);
         }
 
