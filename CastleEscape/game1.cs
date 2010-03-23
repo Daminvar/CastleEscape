8698431c27b1ae0a402d24c1681d7db66166baaf
ohappyfaced <ohappyfaced@hotmail.com>
2010-03-22 21:24:49 -0400

    Changed the color to AliceBlue.

--- CastleEscape/game1.cs
+++ CastleEscape/game1.cs
@@ -75,7 +75,7 @@ namespace CastleEscape
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.AliceBlue);
 
             // TODO: Add your drawing code here
             base.Draw(gameTime);
