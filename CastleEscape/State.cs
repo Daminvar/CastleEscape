7cebc00d7eb54b8050aa4a7e74643029fbb99645
Dennis Honeyman <dmh1117@rit.edu>
2010-03-22 21:03:38 -0400

    Started adding class stubs.

---
+++ CastleEscape/State.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CastleEscape
+{
+    abstract class State
+    {
+        abstract void Initialize();
+        abstract void Pause();
+        abstract void Resume();
+        abstract void Update();
+        abstract void Draw();
+    }
+}
---
+++ CastleEscape/StateManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CastleEscape
+{
+    class StateManager
+    {
+        public void Initialize()
+        {
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Draw()
+        {
+        }
+
+        public void PushState()
+        {
+        }
+
+        public void PopState()
+        {
+        }
+    }
+}
