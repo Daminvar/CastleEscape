be12ecb1d0acb41f1de756b6aa44fdf4daba1ba3
Dennis Honeyman <arcticuno@gmail.com>
2010-03-26 09:29:52 -0400

    Began working on the Overworld state.

---
+++ CastleEscape/Overworld.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace CastleEscape
+{
+    class Overworld : State
+    {
+        public Overworld(Game game) : base(game) { }
+
+        public override void Initialize()
+        {
+        }
+
+        public override void Pause()
+        {
+
+        }
+
+        public override void Resume()
+        {
+
+        }
+
+        public override void Update()
+        {
+
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
