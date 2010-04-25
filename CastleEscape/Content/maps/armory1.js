e967e147348bc7897efb151a4b698a8556d787b0
Dennis Honeyman <arcticuno@gmail.com>
2010-04-25 13:08:26 -0400

    Added second tmx file and initial scripts for armory.

---
+++ CastleEscape/Content/maps/armory1.js
@@ -0,0 +1,8 @@
+
+name("Store Room")
+mapfile("armory1.tmx")
+battleTexture("test-battle-background") // TODO
+
+//TODO
+//north("blah")
+south("armory2.js")
---
+++ CastleEscape/Content/maps/armory2.js
@@ -0,0 +1,8 @@
+
+name("Training Grounds")
+mapfile("armory2.tmx")
+battleTexture("test-battle-background") //TODO
+
+north("armory1.js")
+
+// TODO - Add final boss
