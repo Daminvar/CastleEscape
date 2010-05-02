e678f47c61a3d095244c528c434b068c8a30f538
Dennis Honeyman <arcticuno@gmail.com>
2010-05-02 14:14:08 -0400

    Added gravestone text for graveyard 1

--- CastleEscape/Content/maps/Graveyard1.js
+++ CastleEscape/Content/maps/Graveyard1.js
@@ -20,3 +20,85 @@ axelle.SetInteractFunc(function(player) {
 })
 
 addNPE(axelle)
+
+var grave1 = newNPE()
+grave1.SetPosition(4, 8)
+
+grave1.SetInteractFunc(function(player) {
+	dialogue("Here lies Eugene Nason, who died after attempting to drink fifteen bottles of mead in ten minutes...")
+})
+
+addNPE(grave1)
+
+var grave2 = newNPE()
+grave2.SetPosition(6, 9)
+
+grave2.SetInteractFunc(function(player) {
+	dialogue("Here lies Raphael Marchand, who died in a freak penguin attack. He will always be missed.")
+})
+
+addNPE(grave2)
+
+var grave3 = newNPE()
+grave3.SetPosition(7, 8)
+
+grave3.SetInteractFunc(function(player) {
+	dialogue("Here lies Maxime Lesueur. His death was mostly uneventful...")
+})
+
+addNPE(grave3)
+
+var grave4 = newNPE()
+grave4.SetPosition(12, 7)
+
+grave4.SetInteractFunc(function(player) {
+	dialogue("Here lies Pascal Jacquemin. A true gentleman in both manner and spirit.")
+})
+
+addNPE(grave4)
+
+var grave5 = newNPE()
+grave5.SetPosition(14, 7)
+
+grave5.SetInteractFunc(function(player) {
+	dialogue("HELP, I'M TRAPPED IN A GRAVE CARVING FACTORY!")
+})
+
+addNPE(grave5)
+
+var grave6 = newNPE()
+grave6.SetPosition(12, 3)
+
+grave6.SetInteractFunc(function(player) {
+	dialogue("Here lies Dion Jaillet, who died from injuries after single-handedly defeating eight assassins from the kingdom of Orrz.")
+})
+
+addNPE(grave6)
+
+var grave7 = newNPE()
+grave7.SetPosition(13, 3)
+
+grave7.SetInteractFunc(function(player) {
+	dialogue("Here lies Dion Jaillet's testicles, which were too large to fit in a single casket.")
+})
+
+addNPE(grave7)
+
+function fadedGrave(player) {
+	dialogue("The text is too faded to read.")
+}
+
+var grave8 = newNPE()
+grave8.SetPosition(16, 2)
+grave8.SetInteractFunc(fadedGrave)
+addNPE(grave8)
+
+var grave9 = newNPE()
+grave9.SetPosition(17, 9)
+grave9.SetInteractFunc(fadedGrave)
+addNPE(grave9)
+
+var grave10 = newNPE()
+grave10.SetPosition(18, 3)
+grave10.SetInteractFunc(fadedGrave)
+addNPE(grave10)
