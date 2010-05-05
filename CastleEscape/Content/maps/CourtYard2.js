f2bbbbb21e8bc09486f35395614c72eb34fa771b
unknown <mtm4313@.infotech.it.rit.edu>
2010-05-05 12:52:55 -0400

    Connected flags, added sprites

--- CastleEscape/Content/maps/CourtYard2.js
+++ CastleEscape/Content/maps/CourtYard2.js
@@ -7,7 +7,7 @@ north("Graveyard1.js")
 west("CourtYard1.js")
 
 var courtyard2Civ1= newNPE()
-courtyard2Civ1.SetTexture("test-npe")
+courtyard2Civ1.SetTexture("man_right")
 courtyard2Civ1.SetPosition(9,8)
 
 courtyard2Civ1.SetInteractFunc(function(player) 
@@ -20,17 +20,18 @@ addNPE(courtyard2Civ1)
 
 
 var courtyard2Civ2 = newNPE()
-courtyard2Civ2.SetTexture("test-npe")
+courtyard2Civ2.SetTexture("gGirl-left")
 courtyard2Civ2.SetPosition(12,8)
 
 courtyard2Civ2.SetInteractFunc(function(player) 
 {
 	dialogue("Chris: Nothing quite like reading a book on England's kind and peaceful rulers")	
-
 })
 
 addNPE(courtyard2Civ2)
 
-//TODO: Change textures
-//addRandomEncounter("ghostie", "Fanatic Servant", 100, 50, 10, 10, 30, null)
-//addRandomEncounter("ghostie", "Lazy Guard", 200, 60, 10, 5, 80, null)
+var snake = newEnemy("snake", "snake in your boot", 90, 10, 2, 2, 20, null)
+var ghost = newEnemy("ghostie", "ghost from the past", 100, 9, 2, 3, 22, null)
+
+addRandomEncounter(snake)
+addRandomEncounter(ghost)
