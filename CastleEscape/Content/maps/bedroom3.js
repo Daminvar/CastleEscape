2a93723d4fbd813b29f55399bd643c3c6d2eab97
Dennis Honeyman <arcticuno@gmail.com>
2010-05-05 14:24:31 -0400

    Added save orb to bedroom 3

--- CastleEscape/Content/maps/bedroom3.js
+++ CastleEscape/Content/maps/bedroom3.js
@@ -5,3 +5,14 @@ battleTexture("test-battle-background") //TODO
 
 north("bedroom2.js")
 west("bedroom4.js")
+
+var saveOrb = newNPE()
+saveOrb.SetTexture("orb-of-saving")
+saveOrb.SetPosition(18, 3)
+
+saveOrb.SetInteractFunc(function(player) {
+	save(player)
+	dialogue("Game has been saved.")
+})
+
+addNPE(saveOrb)
