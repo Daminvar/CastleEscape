6c4dbfdaecbb72a7061307291c515667ba9a54b7
Dennis Honeyman <arcticuno@gmail.com>
2010-05-05 14:34:11 -0400

    Added random encounters to the other bedrooms.

--- CastleEscape/Content/maps/bedroom3.js
+++ CastleEscape/Content/maps/bedroom3.js
@@ -16,3 +16,8 @@ saveOrb.SetInteractFunc(function(player) {
 })
 
 addNPE(saveOrb)
+
+var servant = newEnemy("ghostie", "Fanatic Servant", 100, 50, 10, 10, 30, null)
+var lazyGuard = newEnemy("ghostie", "Lazy Guard", 200, 60, 10, 5, 80, null)
+addRandomEncounter(servant)
+addRandomEncounter(lazyGuard)
