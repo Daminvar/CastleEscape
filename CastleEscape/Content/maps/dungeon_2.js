de1389b010a254e7e8d9a3b748f17c571e3c32c4
Dennis Honeyman <arcticuno@gmail.com>
2010-05-12 13:36:07 -0400

    Fixed dungeon 2 enemies.

--- CastleEscape/Content/maps/dungeon_2.js
+++ CastleEscape/Content/maps/dungeon_2.js
@@ -9,7 +9,7 @@ west("dungeon_1.js")
 north("dungeon_3.js")
 south("dungeon_4.js")
 
-var ghost = newEnemy("ghost1_battle", "Ghost of Doom", 30, 7, 1, 1, 10, null)
-var pauper = newEnemy("skeleton1_battle", "Skeleton of Evil", 50, 5, 1, 1, 10, null)
+var ghost = newEnemy("ghost1_battle", "Ghost of Doom", 30, 17, 3, 1, 10, null)
+var pauper = newEnemy("skeleton1_battle", "Skeleton of Evil", 50, 20, 5, 1, 10, null)
 addRandomEncounter(ghost) //Adds a random encounter to the room
 addRandomEncounter(pauper)
